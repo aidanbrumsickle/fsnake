729bb6f430739f5fae19f3c4b3ff0e3f3a0f44c8
Aidan Brumsickle <aidanbrumsickle@gmail.com>
2015-01-31 20:13:08 -0600

    Added stub main.js and a todo file

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// This is the entry point for the app.
+// Nothing here yet, still working on the core functionality.
+//
+// Load required libraries
+// Create the Canvas for the game
+// Create a game instance
+// Do required event binding
+// Start it going
